62546998f147a0eba9306ca2cdd6e9b86b84e2b5
KennethDamCS <mrdam.kiet@gmail.com>
2025-07-10 13:36:55 -0700

    onboarding components and additional packages

--- rideshare/app/(auth)/welcome.tsx
+++ rideshare/app/(auth)/welcome.tsx
@@ -1,7 +1,12 @@
 import {View, Text, SafeAreaView, TouchableOpacity} from "react-native";
 import {router} from "expo-router";
+import Swiper from "react-native-swiper";
+import {useRef, useState} from "react";
+import {onboarding} from "@/constants";
 
 const Onboarding = () => {
+    const swiperRef = useRef<Swiper>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
     return (
         <SafeAreaView className="flex h-full items-center justify-center bg-white">
             <TouchableOpacity onPress={() => {
@@ -10,6 +15,17 @@ const Onboarding = () => {
                               className="w-full flex justify-end items-end p-5">
                 <Text className="text-black text-md font-JakartaBold">Skip</Text>
             </TouchableOpacity>
+            <Swiper ref={swiperRef}
+                    loop={false}
+                    dot={<View className={"w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full"}/>}
+                    activeDot={<View className={"w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full"}/>}
+                    onIndexChanged={(index) => setActiveIndex(index)}>
+                {onboarding.map((item) => (
+                    <View>
+                        <Text>{item.title}</Text>
+                    </View>
+                ))}
+            </Swiper>
         </SafeAreaView>
     )
 };
